977233f6f1242e6b3b759425ea371a4a750f403d
Herman461 <zhukovskiyherman@gmail.com>
2021-10-26 02:50:21 +0400

    Добавлено сабменю

--- form-m/js/app.js
+++ form-m/js/app.js
@@ -317,10 +317,38 @@ document.addEventListener('click', function (e) {
    } else if (
       !targetElement.closest('.search-form__body') &&
       document.querySelector('.search-form__body')
-   ) {
+      ) {
       document.querySelector('.search-form__body').classList.remove('_active');
    }
+   if (targetElement.classList.contains('bottom-header__link')) {
+      e.preventDefault();
+      toggleSubmenu(targetElement);
+   }
 });
+
+
+let currentActiveSubmenu;
+
+function toggleSubmenu(targetElement) {
+   if (!targetElement.nextElementSibling) return;
+   if (currentActiveSubmenu && targetElement.nextElementSibling !== currentActiveSubmenu) {
+      currentActiveSubmenu.hidden = true;
+      currentActiveSubmenu.previousElementSibling.classList.remove('_active');
+   }
+   if (!targetElement.nextElementSibling.classList.contains("_slide")) {
+      if (!targetElement.classList.contains('_active')) {
+         targetElement.nextElementSibling.hidden = true;
+      } else {
+         targetElement.nextElementSibling.hidden = false;
+      }
+      _slideToggle(targetElement.nextElementSibling, 1200);
+      targetElement.classList.toggle("_active");
+      currentActiveSubmenu = targetElement.nextElementSibling;
+   }
+}
+
+
+
 
 function ibg() {
 
